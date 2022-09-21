5267ccec024aecc285590dfbe9df304d683aee92
MajeziBulumko <104558792+MajeziBulumko@users.noreply.github.com>
2022-09-21 09:22:39 +0200

    For Steffen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -3,6 +3,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_ninja/view_by_category.dart';
 
+// the start screen with all the categories.
+// each product has a different categeory.
+// so it only makes sense to view each one
+// by its category
+
 class StartScreen extends StatefulWidget {
   StartScreen({super.key});
   bool loading = false;
@@ -44,6 +49,7 @@ class _StartScreenState extends State<StartScreen> {
                       final String mane = name.text;
                       if (mane != null) {
                         await _cats.doc(docSnap!.id).update({"category": mane});
+                        name.text = '';
                       }
                     },
                     child: const Text('update'))
@@ -61,45 +67,103 @@ class _StartScreenState extends State<StartScreen> {
         const SnackBar(content: Text('Category has been deleted')));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: _cats.snapshots(),
-      builder: (context, AsyncSnapshot<QuerySnapshot> streamSnap) {
-        if (streamSnap.hasData) {
-          return ListView.builder(
-            itemCount: streamSnap.data!.docs.length,
-            itemBuilder: (context, index) {
-              final DocumentSnapshot docSnap = streamSnap.data!.docs[index];
-              return InkWell(
-                child: ListTile(
-                  title: Text(docSnap['category']),
-                  trailing: SizedBox(
-                    width: 100,
-                    child: Row(
-                      children: [
-                        IconButton(
-                            onPressed: () => _update(docSnap),
-                            icon: const Icon(Icons.edit)),
-                        IconButton(
-                            onPressed: () => _delete(docSnap.id),
-                            icon: const Icon(Icons.delete)),
-                        IconButton(
-                            onPressed: () => const ViewCat(),
-                            icon: const Icon(Icons.add_home))
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
-          );
-        }
+  Future<void> _create([DocumentSnapshot? docSnap]) async {
+    await showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext bctx) {
+        return Padding(
+          padding: EdgeInsets.only(
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: MediaQuery.of(bctx).viewInsets.bottom + 20,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: name,
+                decoration: const InputDecoration(labelText: 'Category'),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                  onPressed: () async {
+                    final String mane = name.text;
 
-        return const Center(
-          child: CircularProgressIndicator(),
+                    if (mane != null) {
+                      await _cats.add({"category": mane});
+                      name.text = '';
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child: const Text('Create'))
+            ],
+          ),
         );
       },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Categories'),
+        ),
+      ),
+      body: StreamBuilder(
+        stream: _cats.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnap) {
+          if (streamSnap.hasData) {
+            return ListView.builder(
+              itemCount: streamSnap.data!.docs.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot docSnap = streamSnap.data!.docs[index];
+                String catego = docSnap['category'];
+                return Card(
+                  child: InkWell(
+                    onTap: () {
+                      // I wanna lead it to the view by category page.
+                      return ViewCat(category: catego);
+                    },
+                    child: ListTile(
+                      onTap: (() async {
+                        ViewCat(category: catego);
+                      }),
+                      title: Text(docSnap['category']),
+                      trailing: SizedBox(
+                        width: 150,
+                        child: Row(
+                          children: [
+                            IconButton(
+                                onPressed: () => _update(docSnap),
+                                icon: const Icon(Icons.edit)),
+                            IconButton(
+                                onPressed: () => _delete(docSnap.id),
+                                icon: const Icon(Icons.delete)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (() => _create()),
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
